95cc5caac0dc2f5080f64d00ef64e6c777fcd69a
kokivanov <kokivanov@gmail.com>
2021-03-18 20:29:25 +0000

    commandtemplate update

--- utils/commandtemplate.py
+++ utils/commandtemplate.py
@@ -1,15 +1,20 @@
 import asyncio
 import json
-from . import exceptions as exc
+
+if __name__ == '__main__':
+    from exceptions import *
+    import types
+else:
+    from .exceptions import *
 
 class Command(object):
     name = ''
     description = ''
     usage = ''
-    parameters = []
+    parameters = {}
     aliases = []
     is_callable = True
-    required_permissions = {}
+    required_permissions = []
     channels_blacklist = []
     roles_blacklist = []
 
@@ -17,18 +22,39 @@ class Command(object):
 
     def __init__(self, *args, **kwargs):
         try:
-            self.name = kwargs['name']
-            self.description = kwargs['description']
-            self.parameters = kwargs['parameters']
-            self.permissions = kwargs['permissions']
-            self.aliases = kwargs['aliases']
-            self.is_callable = kwargs['is_callable']
-            self.required_permissions = kwargs['required_permissions']
-            self.channels_blacklist = kwargs['channels_blacklist']
-            self.roles_blacklist = kwargs['roles_blacklist']
-            self.command = kwargs['command']
-        except (KeyError):
+            if not isinstance(kwargs.get('name'), str):
+                raise TypeError('Required type str as a name')
+            if not isinstance(kwargs.get('description'), str):
+                raise TypeError('Required type str as a description')
+            if not isinstance(kwargs.get("usage"), str):
+                raise TypeError('Required type str as a usage')
+            if not isinstance(kwargs.get("parameters"), dict):
+                raise TypeError('Required type dict as a parameters')
+            if not isinstance(kwargs.get("aliases"), list):
+                raise TypeError('Required type list as a aliases')
+            if not isinstance(kwargs.get("is_callable"), bool):
+                raise TypeError('Required type bool as a is_callable')
+            if not isinstance(kwargs.get("required_permissions"), list):
+                raise TypeError('Required type list as a required_permissions')
+            if not isinstance(kwargs.get("channels_blacklist"), list):
+                raise TypeError('Required type list as a channels_blacklist')
+            if not isinstance(kwargs.get("roles_blacklist"), list):
+                raise TypeError('Required type list as a roles_blacklist')
+
+            self.name = kwargs.get('name')
+            self.description = kwargs.get('description')
+            self.usage = kwargs.get("usage")
+            self.parameters = kwargs.get('parameters')
+            self.aliases = kwargs.get('aliases')
+            self.is_callable = kwargs.get('is_callable')
+            self.required_permissions = kwargs.get('required_permissions')
+            self.channels_blacklist = kwargs.get('channels_blacklist')
+            self.roles_blacklist = kwargs.get('roles_blacklist')
+            self.command = kwargs.get('command')
+        except KeyError:
             print("Can't recognize some parameters.")
+        except TypeError as e:
+            print("Invalid parameters provided: {}".format(e))
 
     async def __call__(self, *args, **kwargs):
         await self.command(*args, **kwargs)
@@ -38,7 +64,6 @@ class Command(object):
             "name" : self.name,
             "description" : self.description,
             "parameters" : self.parameters,
-            "permissions" : self.permissions,
             "aliases" : self.aliases,
             "is_callable" : self.is_callable,
             "required_permissions" : self.required_permissions,
@@ -53,58 +78,172 @@ class Command(object):
         self.name = par['name']
         self.description = par['description']
         self.parameters = par['parameters']
-        self.permissions = par['permissions']
         self.aliases = par['aliases']
         self.is_callable = par['is_callable']
         self.required_permissions = par['required_permissions']
         self.channels_blacklist = par['channels_blacklist']
         self.roles_blacklist = par['roles_blacklist']
-        
-        pass
 
     def __str__(self):
         return "Command {}, {}".format(self.name, self.description)
 
-    def set_permission(self, *args, **kwargs):
-        try:
-            if not isinstance(args[0], dict):
-                err = exc.InvalidParameter(command='{} : premission set'.format(self.__class__.__name__))
-                print(err)
-                raise err
-            self.permissions = args[0]
-        except KeyError:
-            err = exc.InvalidParameter(command='{} : premission set'.format(self.__class__.__name__))
-            print(err)
-            raise err
+    def set_permissions(self, *args, **kwargs):
+        if len(args) < 1 or '' in args:
+            raise ValueError("Can't append no parameters")
+        for i in args:
+            if not isinstance(i, str):
+                raise InvalidParameter(command='{} : premission add'.format(self.__class__.__name__), has_param=args, required_param=["any amount of str"], msg="Incorrect parameters")
 
-    # def add_permission(self, *args, **kwargs)
-    # def remove_permission(self, *args, **kwargs)
-    # def prune_permission(self, *args, **kwargs)
+        self.required_permissions = args
+
+    def add_permissions(self, *args, **kwargs):
+        if len(args) < 1 or '' in args:
+            raise ValueError("Can't append no parameters")
+        for i in args:
+            if isinstance(i, str):
+                if i in self.required_permissions:
+                    pass
+                else:
+                    self.required_permissions.append(i)
+            else:
+                raise InvalidParameter(command='{} : premission add'.format(self.__class__.__name__), has_param=args, required_param=["any amount of str"], msg="Incorrect parameters")
     
-    # def set_aliases(self, *args, **kwargs)
-    # def add_aliases(self, *args, **kwargs)
-    # def remove_aliases(self, *args, **kwargs)
-    # def prune_aliases(self, *args, **kwargs)
+    def remove_permissions(self, *args, **kwargs):
+        if len(args) < 1:
+            raise ValueError("Can't remove void from list")
+        for i in args:
+            if isinstance(i, str):
+                if i in self.required_permissions:
+                    self.required_permissions.remove(i)
+                else:
+                    pass
+            else:
+                raise InvalidParameter(command='{} : premission remove'.format(self.__class__.__name__), has_param=args, required_param=["any amount of str"], msg="Incorrect parameters")
 
-    # def set_availability(self, *args, **kwargs)
-    # def change_availability(self, *args, **kwargs)
+    def prune_permissions(self, *args, **kwargs):
+        self.required_permissions.clear()
+
+    def set_aliases(self, *args, **kwargs):
+        if len(args) < 1 or '' in args:
+            raise ValueError("Can't append no parameters")
+        for i in args:
+            if not isinstance(i, str):
+                raise InvalidParameter(command='{} : premission add'.format(self.__class__.__name__), has_param=args, required_param=["any amount of str"], msg="Incorrect parameters")
+
+        self.aliases = list(args)
+
+    def add_aliases(self, *args, **kwargs):
+        if len(args) < 1 or '' in args:
+            raise ValueError("Can't append no parameters")
+        for i in args:
+            if isinstance(i, str):
+                if i in self.aliases:
+                    pass
+                else:
+                    self.aliases.append(i)
+            else:
+                raise InvalidParameter(command='{} : aliases add'.format(self.__class__.__name__), has_param=args, required_param=["any amount of str"], msg="Incorrect parameters")
+
+    def remove_aliases(self, *args, **kwargs):
+        if len(args) < 1:
+            raise ValueError("Can't remove void from list")
+        for i in args:
+            if isinstance(i, str):
+                if i in self.aliases:
+                    self.aliases.remove(i)
+                else:
+                    pass
+            else:
+                raise InvalidParameter(command='{} : aliases remove'.format(self.__class__.__name__), has_param=args, required_param=["any amount of str"], msg="Incorrect parameters")
     
-    # def set_channel_blacklist(self, *args, **kwargs)
-    # def add_channel_blacklist(self, *args, **kwargs)
-    # def remove_channel_blacklist(self, *args, **kwargs)
-    # def prune_channel_blacklist(self, *args, **kwargs)
+    def prune_aliases(self, *args, **kwargs):
+        self.aliases.clear()
 
-    # def set_role_blacklist(self, *args, **kwargs)
-    # def add_role_blacklist(self, *args, **kwargs)
-    # def remove_role_blacklist(self, *args, **kwargs)
-    # def prune_role_blacklist(self, *args, **kwargs)
+    def set_availability(self, *args, **kwargs):
+        if len(args) >= 1 and isinstance(args[0], bool):
+            self.is_callable = args[0]
+
+    def change_availability(self, *args, **kwargs):
+        self.is_callable ^= True
+    
+    def set_channels_blacklist(self, *args, **kwargs):
+        if len(args) < 1 or '' in args:
+            raise ValueError("Can't append no parameters")
+        for i in args:
+            if not isinstance(i, str):
+                raise InvalidParameter(command='{} : premission add'.format(self.__class__.__name__), has_param=args, required_param=["any amount of str"], msg="Incorrect parameters")
+
+        self.channels_blacklist = list(args)
+
+    def add_channels_blacklist(self, *args, **kwargs):
+        if len(args) < 1 or '' in args:
+            raise ValueError("Can't append no parameters")
+        for i in args:
+            if isinstance(i, str):
+                if i in self.channels_blacklist:
+                    pass
+                else:
+                    self.channels_blacklist.append(i)
+            else:
+                raise InvalidParameter(command='{} : channels_blacklist add'.format(self.__class__.__name__), has_param=args, required_param=["any amount of str"], msg="Incorrect parameters")
+
+    def remove_channels_blacklist(self, *args, **kwargs):
+        if len(args) < 1:
+            raise ValueError("Can't remove void from list")
+        for i in args:
+            if isinstance(i, str):
+                if i in self.channels_blacklist:
+                    self.channels_blacklist.remove(i)
+                else:
+                    pass
+            else:
+                raise InvalidParameter(command='{} : channels_blacklist remove'.format(self.__class__.__name__), has_param=args, required_param=["any amount of str"], msg="Incorrect parameters")
+    
+    def prune_channels_blacklist(self, *args, **kwargs):
+        self.channels_blacklist.clear()
+
+    def set_roles_blacklist(self, *args, **kwargs):
+        if len(args) < 1 or '' in args:
+            raise ValueError("Can't append no parameters")
+        for i in args:
+            if not isinstance(i, str):
+                raise InvalidParameter(command='{} : premission add'.format(self.__class__.__name__), has_param=args, required_param=["any amount of str"], msg="Incorrect parameters")
+
+        self.roles_blacklist = list(args)
+
+    def add_roles_blacklist(self, *args, **kwargs):
+        if len(args) < 1 or '' in args:
+            raise ValueError("Can't append no parameters")
+        for i in args:
+            if isinstance(i, str):
+                if i in self.roles_blacklist:
+                    pass
+                else:
+                    self.roles_blacklist.append(i)
+            else:
+                raise InvalidParameter(command='{} : roles_blacklist add'.format(self.__class__.__name__), has_param=args, required_param=["any amount of str"], msg="Incorrect parameters")
+
+    def remove_roles_blacklist(self, *args, **kwargs):
+        if len(args) < 1:
+            raise ValueError("Can't remove void from list")
+        for i in args:
+            if isinstance(i, str):
+                if i in self.roles_blacklist:
+                    self.roles_blacklist.remove(i)
+                else:
+                    pass
+            else:
+                raise InvalidParameter(command='{} : roles_blacklist remove'.format(self.__class__.__name__), has_param=args, required_param=["any amount of str"], msg="Incorrect parameters")
+    
+    def prune_roles_blacklist(self, *args, **kwargs):
+        self.roles_blacklist.clear()
 
 def main():
     cmd = Command(
         name='test',
         description='test',
+        usage="a-test",
         parameters={'Test':False},
-        permissions=[],
         aliases = [],
         is_callable= True,
         required_permissions = [],
@@ -112,14 +251,8 @@ def main():
         roles_blacklist = [],
         command= None
     )
-
-    print(cmd)
-
-    p = ~cmd
-    print(p)
-
-    cmd << '{"name" : "self.name","description" : "self.description","parameters" : {"a" : false},"permissions" : [],"aliases" : [],"is_callable" : true,"required_permissions" : [],"channels_blacklist" : [],"roles_blacklist" : []}'
-    print(cmd)
-
+    
 if __name__ == '__main__':
     main()
+
+    print("Hi!")
--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,36 +1,38 @@
 import datetime
 
-class ErrorTemplate(object):
+class ErrorTemplate(Exception):
     errorTime = None
     errorHash = None
     command = None
 
     def __init__(self, *args, **kwargs):
-        self.errorTime = datetime.now()
-        self.errorHash = hash(errorTime)
+        self.errorTime = datetime.datetime.now()
+        self.errorHash = hash(self.errorTime)
         try:
             self.command = kwargs.get('command')
-        except:
+            super().__init__(kwargs.get("msg"))
+        except KeyError:
+            raise Exception("Must specify a message")
             pass
-        super().__init__(*args, **kwargs)
 
     def __str__(self):
         return "{} error, at {}\n Hash : {}\n".format(self.__class__.__name__, self.errorTime, self.errorHash)
 
-class ArgumentErr(object, ErrorTemplate):
+class ArgumentErr(ErrorTemplate):
     required_args = []
     has_args = []
 
     def __init__(self, *args, **kwargs):
         self.required_args = kwargs['required_args']
         self.has_args = kwargs['has_args']
+        super().__init__(*args, **kwargs)
 
     pass
 
-class ConnectionErr(object, ErrorTemplate):
+class ConnectionErr(ErrorTemplate):
     pass
 
-class PermissionErr(object, ErrorTemplate):
+class PermissionErr(ErrorTemplate):
     reqired_permissions = []
     has_permission = []
 
@@ -38,13 +40,31 @@ class PermissionErr(object, ErrorTemplate):
         self.required_premissions = kwargs['required_premissions']
         self.required_premissions = kwargs['has_premissions']
 
+        super().__init__(*args, **kwargs)
+
     def __str__(self):
         return "{}Reqired permissions: {}\nHas permissions: {}\n".format(super().__str__(), self.required_premissions, self.has_permission)
 
     pass
 
-class InvalidConfig(object, ErrorTemplate):
+class InvalidConfig(ErrorTemplate):
     pass
 
-class InvalidParameter(object, ErrorTemplate):
+class InvalidParameter(ErrorTemplate):
+    required_param = []
+    has_param = []
+
+    def __str__(self):
+        return "{}\nRequired: {}\n got: {}\n".format(super().__str__(), self.required_param, self.has_param)
+
+    def __init__(self, *args, **kwargs):
+        try:
+            self.required_param = kwargs.get('required_param')
+            self.has_param = kwargs.get('has_param')
+
+        except KeyError as ex:
+            print("Invalid parameters: {}\n".format(ex.__cause__)) 
+
+        super().__init__(*args, **kwargs)
+
     pass
